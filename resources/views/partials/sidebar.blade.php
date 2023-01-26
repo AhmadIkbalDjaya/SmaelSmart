fedac2281b9453fdd2ae478e3130aed42ee2049c
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-01-26 10:20:38 +0700

    Mengatur active page sidebar dan membuat model user

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,17 +5,17 @@
     <h4 class="align-self-center">username</h4>
   </div>
   <ul class="content-sidebar main-content-sidebar">
-    <li><a href="/" class="active-page"><i class="fa-solid fa-house"></i>Home</a></li>
-    <li><a href="/calender"><i class="fa-regular fa-calendar-days"></i>Kalender</a></li>
-    <li><a href="/control-card"><i class="fa-solid fa-address-card"></i>Kartu Kontrol</a></li>
-    <li><a href="/raport"><i class="fa-sharp fa-solid fa-book"></i>Raport</a></li>
+    <li><a href="/" class="{{ Request::is('/') ? 'active-page' : '' }}"><i class="fa-solid fa-house"></i>Home</a></li>
+    <li><a href="/calender" class="{{ Request::is('calender') ? 'active-page' : '' }}"><i class="fa-regular fa-calendar-days"></i>Kalender</a></li>
+    <li><a href="/control-card" class="{{ Request::is('control-card') ? 'active-page' : '' }}"><i class="fa-solid fa-address-card"></i>Kartu Kontrol</a></li>
+    <li><a href="/raport" class="{{ Request::is('raport') ? 'active-page' : '' }}"><i class="fa-sharp fa-solid fa-book"></i>Raport</a></li>
   </ul>
   <hr class="mx-2 line-sidebar">
   <ul class="content-sidebar lesson-content-sidebar">
-    <li><a href="/lesson"><i class="fa-solid fa-book"></i> Matematika</a></li>
-    <li><a href=""><i class="fa-solid fa-book"></i> Fisika</a></li>
-    <li><a href=""><i class="fa-solid fa-book"></i> Kimia</a></li>
-    <li><a href=""><i class="fa-solid fa-book"></i> Bahasa Inggris</a></li>
+    <li><a href="/lesson" class="{{ Request::is('lesson') ? 'active-page' : '' }}"><i class="fa-solid fa-book"></i> Matematika</a></li>
+    <li><a href="" ><i class="fa-solid fa-book"></i> Fisika</a></li>
+    <li><a href="" ><i class="fa-solid fa-book"></i> Kimia</a></li>
+    <li><a href="" ><i class="fa-solid fa-book"></i> Bahasa Inggris</a></li>
   </ul>
 </div>
 <!-- end sidebar -->
